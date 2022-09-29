8a5aae4579d5cfb90f2b64bc635c57c1417d062c
saddaul-siam <saddadul.siam@gmail.com>
2022-09-29 17:11:02 +0600

    fixed category

--- components/sharedComponents/HeadlessUIDropDown/HeadlessUICategoriesDPD.jsx
+++ components/sharedComponents/HeadlessUIDropDown/HeadlessUICategoriesDPD.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import categories from "../../../images/categories";
+import Image from "next/image";
 
 const HeadlessUICategoriesDPD = () => {
   function classNames(...classes) {
@@ -25,23 +27,24 @@ const HeadlessUICategoriesDPD = () => {
       leaveFrom="transform opacity-100 scale-100"
       leaveTo="transform opacity-0 scale-95"
     >
-      <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+      <Menu.Items className="absolute left-0 z-10 mt-2 w-[17.5rem] origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
         <div className="py-1">
-          {dropdownMenu.map((menu, index) => (
+          {categories.map((category, index) => (
             <Menu.Item key={index}>
               {({ active }) => (
                 <a
                   href="#"
                   className={classNames(
                     active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                    "block px-4 py-2 text-sm"
+                    "block  px-4 py-2 text-sm"
                   )}
                 >
                   <div className="flex justify-between">
-                    <span>{menu.name}</span>
-                    <span>
-                      <MdKeyboardArrowRight className="text-xl" />
-                    </span>
+                    <div className="flex space-x-3">
+                      <Image src={category.img} alt="" width="20" height="20" />
+                      <p>{category.name}</p>
+                    </div>
+                    <MdKeyboardArrowRight className="text-xl" />
                   </div>
                 </a>
               )}
