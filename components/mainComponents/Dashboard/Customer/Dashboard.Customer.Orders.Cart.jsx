e99d02f389a3d6140cf1dabb1a696af6b851ba7d
saddaul-siam <saddadul.siam@gmail.com>
2022-10-02 13:45:05 +0600

    code update

--- components/mainComponents/Dashboard/Customer/Dashboard.Customer.Orders.Cart.jsx
+++ components/mainComponents/Dashboard/Customer/Dashboard.Customer.Orders.Cart.jsx
@@ -6,26 +6,23 @@ const DashboardCustomerOrdersCart = ({ orders }) => {
   return (
     <>
       {orders.slice(0, 3).map(({ id, status, date, total }, i) => (
-        <ul
-          key={i}
-          className="my-3 grid grid-cols-4 rounded-md bg-white py-5 px-5 text-base font-light text-gray-600 shadow"
-        >
-          <li className="text-lg text-black">{id}</li>
-          <li>
-            <p className="inline rounded-full bg-slate-200 py-1 px-3 text-sm">
-              {status}
-            </p>
-          </li>
-          <li>{date}</li>
-          <li className="flex justify-between">
-            <span>$ {total}</span>
-            <Link href={`/dashboard/customer/orders/${id}`}>
+        <Link key={i} href={`/dashboard/customer/orders/${id}`}>
+          <ul className="my-3 grid cursor-pointer grid-cols-4 rounded-md bg-white py-5 px-5 text-base font-light text-gray-600 shadow">
+            <li className="text-lg text-black">{id}</li>
+            <li>
+              <p className="inline rounded-full bg-slate-200 py-1 px-3 text-sm">
+                {status}
+              </p>
+            </li>
+            <li>{date}</li>
+            <li className="flex justify-between">
+              <span>$ {total}</span>
               <span className="cursor-pointer rounded-full p-2 text-xl transition duration-200 ease-in-out hover:bg-slate-200">
                 <AiOutlineArrowRight />
               </span>
-            </Link>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </Link>
       ))}
     </>
   );
