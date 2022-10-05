69ae10a38dc19e8dad70e5123994c8350a94d163
saddaul-siam <saddadul.siam@gmail.com>
2022-10-05 23:44:40 +0600

    create modal

---
+++ components/sharedComponents/modal/Products.Modal.jsx
@@ -0,0 +1,127 @@
+import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { Fragment } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+export default function ProductsModal({ setIsOpen, isOpen }) {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setIsOpen(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Dialog.Panel
+                  className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white
+                p-5 text-left align-middle shadow-xl transition-all lg:max-w-2xl"
+                >
+                  <div className="grid grid-cols-2 gap-5">
+                    <div className="">
+                      <Image
+                        className="rounded-md"
+                        height="350"
+                        width="250"
+                        src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                        alt=""
+                      />
+                      {/* images */}
+                      <div className="flex gap-x-2">
+                        <Image
+                          className="cursor-pointer rounded-md"
+                          height="100"
+                          width="78"
+                          src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                          alt=""
+                        />
+                        <Image
+                          className="cursor-pointer rounded-md"
+                          height="100"
+                          width="78"
+                          src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                          alt=""
+                        />
+                        <Image
+                          className="cursor-pointer rounded-md"
+                          height="100"
+                          width="78"
+                          src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div className=""></div>
+                    </div>
+                    <div className="space-y-1.5 text-sm text-gray-500 sm:space-y-3">
+                      <h2 className="pb-3 text-xl font-bold text-black sm:text-3xl">
+                        Say Ban Ocean
+                      </h2>
+                      <p className="">
+                        Brand:{" "}
+                        <span className="font-semibold text-gray-700">
+                          Ziaomi
+                        </span>
+                      </p>
+                      <p className="">
+                        Rated:🥰🥰🥰🥰🥰
+                        <span className="font-semibold text-gray-700">
+                          {" "}
+                          (50)
+                        </span>
+                      </p>
+                      <p className="">Stock Available</p>
+                      <p className="pt-3 text-2xl font-bold text-red-500/90 sm:pt-5 sm:text-3xl">
+                        $250.00
+                      </p>
+                      <div className="flex flex-col space-y-4 py-3 sm:flex-row sm:space-x-5 sm:py-5">
+                        <div className="flex items-center space-x-2 text-base">
+                          <span className="cursor-pointer rounded-md border border-red-500 p-1 text-red-500 hover:bg-red-500 hover:text-white">
+                            <AiOutlineMinus />
+                          </span>
+                          <span>1</span>
+                          <span className="cursor-pointer rounded-md border border-red-500 p-1 text-red-500 hover:bg-red-500 hover:text-white">
+                            <AiOutlinePlus />
+                          </span>
+                        </div>
+                        <button className="ease-in-outs rounded-md bg-red-500/90 px-10 py-2 text-white transition duration-300 hover:bg-red-600">
+                          Add to cart
+                        </button>
+                      </div>
+                      <p className="">
+                        Sold By:{" "}
+                        <span className="font-semibold text-gray-700">
+                          Mobile Store
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </div>
+            </div>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
--- components/sharedComponents/productsCard/Products.Card.jsx
+++ components/sharedComponents/productsCard/Products.Card.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import {
   AiFillEye,
   AiFillStar,
@@ -9,74 +9,80 @@ import {
   AiOutlineStar,
 } from "react-icons/ai";
 import { BsCart2 } from "react-icons/bs";
+import ProductsModal from "../modal/Products.Modal";
 
 const ProductsCard = ({ product }) => {
+  let [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <Link href={`/product/${product.id}`}>
-        <div className="relative">
-          <div
-            key={product.id}
-            className="group relative w-full rounded-md bg-white pb-1"
-          >
-            <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-white group-hover:opacity-60 lg:aspect-none lg:h-80">
-              <img
-                src={product.imageSrc}
-                alt={product.imageAlt}
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full "
-              />
-            </div>
-            <div className="m-2 mt-4 flex justify-between">
-              <div className="space-y-2">
-                <h3 className="text-base font-semibold text-gray-600">
-                  <a href={product.href}>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {product.name}
-                  </a>
-                </h3>
-                <p className="flex text-base text-yellow-400 ">
-                  <AiFillStar />
-                  <AiFillStar />
-                  <AiFillStar />
-                  <AiFillStar />
-                  <AiOutlineStar />
-                </p>
-                <div className="flex space-x-2 text-base font-medium">
-                  <p className="text-red-600/90">{product.price}</p>
-                  {/* Discount price */}
-                  <p className="text-gray-600 line-through">{product.price}</p>
-                </div>
-              </div>
-              {/* Add to Cart */}
-              <div className="z-10 flex flex-col items-center justify-center text-base ">
-                <span className="cursor-pointer rounded-md border border-red-500 p-1 text-red-500 hover:bg-red-500 hover:text-white">
-                  <AiOutlineMinus />
-                </span>
-                1
-                <span className="cursor-pointer rounded-md border border-red-500 p-1 text-red-500 hover:bg-red-500 hover:text-white">
-                  <AiOutlinePlus />
-                </span>
+      {/* <Link href={`/product/${product.id}`}> */}
+      <div className="relative">
+        <div
+          key={product.id}
+          className="group relative w-full rounded-md bg-white pb-1"
+        >
+          <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-white group-hover:opacity-60 lg:aspect-none lg:h-80">
+            <img
+              src={product.imageSrc}
+              alt={product.imageAlt}
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full "
+            />
+          </div>
+          <div className="m-2 mt-4 flex justify-between">
+            <div className="space-y-2">
+              <h3 className="text-base font-semibold text-gray-600">
+                <a href={product.href}>
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  {product.name}
+                </a>
+              </h3>
+              <p className="flex text-base text-yellow-400 ">
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiOutlineStar />
+              </p>
+              <div className="flex space-x-2 text-base font-medium">
+                <p className="text-red-600/90">{product.price}</p>
+                {/* Discount price */}
+                <p className="text-gray-600 line-through">{product.price}</p>
               </div>
             </div>
-            {/* Card Discount */}
-            <div className="absolute top-2 left-2 rounded-full bg-red-500/90 px-2 py-0.5 text-sm text-white">
-              50% off
-            </div>
-            {/* Card Icons */}
-            <div className="absolute top-1/2 left-1/2 hidden -translate-x-1/2 -translate-y-1/2 transform space-x-3 text-xl text-gray-900 group-hover:flex">
-              <span className="cursor-pointer rounded-full bg-gray-300 p-2">
-                <AiFillEye className="modal-button" />
+            {/* Add to Cart */}
+            <div className="z-10 flex flex-col items-center justify-center text-base ">
+              <span className="cursor-pointer rounded-md border border-red-500 p-1 text-red-500 hover:bg-red-500 hover:text-white">
+                <AiOutlineMinus />
               </span>
-              <span className="cursor-pointer rounded-full bg-gray-300 p-2">
-                <AiOutlineHeart />
-              </span>
-              <span className="cursor-pointer rounded-full bg-gray-300 p-2">
-                <BsCart2 />
+              1
+              <span className="cursor-pointer rounded-md border border-red-500 p-1 text-red-500 hover:bg-red-500 hover:text-white">
+                <AiOutlinePlus />
               </span>
             </div>
           </div>
+          {/* Card Discount */}
+          <div className="absolute top-2 left-2 rounded-full bg-red-500/90 px-2 py-0.5 text-sm text-white">
+            50% off
+          </div>
+          {/* Card Icons */}
+          <div className="absolute top-1/2 left-1/2 hidden -translate-x-1/2 -translate-y-1/2 transform space-x-3 text-xl text-gray-900 group-hover:flex">
+            <button
+              className="cursor-pointer rounded-full bg-gray-300 p-2"
+              onClick={() => setIsOpen(true)}
+            >
+              <AiFillEye className="modal-button" />
+            </button>
+            <button className="cursor-pointer rounded-full bg-gray-300 p-2">
+              <AiOutlineHeart />
+            </button>
+            <button className="cursor-pointer rounded-full bg-gray-300 p-2">
+              <BsCart2 />
+            </button>
+          </div>
         </div>
-      </Link>
+      </div>
+      {/* </Link> */}
+      <ProductsModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   );
 };
