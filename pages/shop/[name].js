b045b35a6f808f19560ef2ce5ac0abe2598dcbe8
saddaul-siam <saddadul.siam@gmail.com>
2022-10-08 13:32:57 +0600

    complict vengor store page

--- pages/shop/[name].js
+++ pages/shop/[name].js
@@ -14,17 +14,25 @@ import {
   ProductCard,
   SwiperProductCard,
 } from "../../components/sharedComponents";
-import { SearchingProductsSidebarMenu } from "../../components/mainComponents/SearchingProducts";
+import {
+  SearchingProductsSidebar,
+  SearchingProductsSidebarMenu,
+} from "../../components/mainComponents/SearchingProducts";
 import { products } from "../../data/products";
+import { BiCategory } from "react-icons/bi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FiFilter } from "react-icons/fi";
 
 const Name = () => {
   const [selectCategory, setSelectCategory] = useState("Home Page");
+  const [showSidebar, setShowSidebar] = useState(null);
   const categories = ["Home Page", "All Products", "Profile"];
   return (
     <>
       <Navbar />
       <div className="mt-[7.7rem] lg:mt-44">
         <div className="container py-10">
+          {/* Banner */}
           <div className="rounded-lg bg-white">
             <div className="relative">
               {/* Banner */}
@@ -47,8 +55,8 @@ const Name = () => {
                 />
               </div>
             </div>
-            <div className="flex justify-between pt-3 pb-5 ">
-              <div className="ml-40 space-y-1 text-base text-gray-500">
+            <div className="mx-3 flex justify-between pt-10 pb-5 md:pt-3">
+              <div className="space-y-1 text-base text-gray-500 md:ml-32 lg:ml-40 ">
                 <h2 className="text-xl font-semibold text-gray-600">
                   Store Name
                 </h2>
@@ -68,7 +76,7 @@ const Name = () => {
                   <AiFillPhone className="inline" /> 01311333277
                 </p>
               </div>
-              <div className="mr-10 flex items-center">
+              <div className="flex items-center md:mr-10">
                 <button className="rounded-md border-2 border-red-500 py-2 px-10 transition duration-300 ease-in-out hover:bg-red-500 hover:text-white">
                   Contact
                 </button>
@@ -76,7 +84,7 @@ const Name = () => {
             </div>
           </div>
           {/* filter */}
-          <div className="my-3 flex-1 justify-between bg-white p-5 sm:flex">
+          <div className="my-3 flex-1 justify-between space-y-5 bg-white p-5 sm:flex">
             <div className="tabs">
               {categories.map((category) => (
                 <a
@@ -214,6 +222,43 @@ const Name = () => {
                 </div>
               </div>
               <div className="col-span-5 lg:col-span-4">
+                <div className="mb-3 flex-1 justify-between space-y-5 rounded-md bg-white p-5 md:flex">
+                  <div className="flex items-center ">
+                    <h2 className="text-lg font-semibold text-gray-800">
+                      Store Name
+                    </h2>
+                  </div>
+                  <div className="flex items-center space-x-3 text-sm text-gray-600 sm:space-x-4 sm:text-base">
+                    <p>Short by:</p>
+                    <select
+                      name=""
+                      id=""
+                      className="h-10 rounded border border-gray-300 focus:border-red-500"
+                    >
+                      <option value="" className="">
+                        Best Match
+                      </option>
+                      <option value="">Date</option>
+                      <option value="">Price low to high</option>
+                      <option value="">Price high to low</option>
+                    </select>
+                    <p
+                      className="cursor-pointer rounded-full p-2 text-lg hover:bg-slate-200 sm:text-xl lg:hidden"
+                      onClick={() => setShowSidebar(true)}
+                    >
+                      <FiFilter />
+                    </p>
+                    <div className="flex items-center space-x-1">
+                      <p>View:</p>
+                      <p className="cursor-pointer rounded-full p-2 text-lg text-red-500/90 hover:bg-slate-200 sm:text-xl">
+                        <BiCategory />
+                      </p>
+                      <p className="cursor-pointer rounded-full p-2 text-lg hover:bg-slate-200 sm:text-xl">
+                        <GiHamburgerMenu />
+                      </p>
+                    </div>
+                  </div>
+                </div>
                 <div className="grid grid-cols-2 gap-x-2 gap-y-5 rounded-md sm:grid-cols-3 sm:gap-x-5 lg:grid-cols-4">
                   {products.slice(0, 12).map((product, index) => (
                     <ProductCard product={product} key={index} />
@@ -225,6 +270,9 @@ const Name = () => {
           {selectCategory === "Profile" && <h1>Developing on the way</h1>}
         </div>
       </div>
+      {showSidebar && (
+        <SearchingProductsSidebar setShowSidebar={setShowSidebar} />
+      )}
       <BottomBar />
       <Footer />
     </>
