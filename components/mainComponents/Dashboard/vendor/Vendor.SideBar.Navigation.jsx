6a42aa46ca8ac8c6b2fcc2673f763de36ba20501
saddaul-siam <saddadul.siam@gmail.com>
2022-10-10 22:39:24 +0600

    added link

--- components/mainComponents/Dashboard/vendor/Vendor.SideBar.Navigation.jsx
+++ components/mainComponents/Dashboard/vendor/Vendor.SideBar.Navigation.jsx
@@ -4,13 +4,16 @@ import { MdDashboard, MdOutlineArrowForwardIos } from "react-icons/md";
 import { BsCardText, BsCart2, BsUpload } from "react-icons/bs";
 import { AiOutlineHome, AiOutlineSetting } from "react-icons/ai";
 import logo from "../../../../images/logo/logo.svg";
+import { useRouter } from "next/router";
 
 const VendorSideBarNavigation = () => {
+  const router = useRouter();
   const dashboard = [
     {
       title: "Dashboard",
       icon: <MdDashboard />,
       arrow: <MdOutlineArrowForwardIos />,
+      href: "/vendor/dashboard",
     },
   ];
   const pages = [
@@ -18,21 +21,25 @@ const VendorSideBarNavigation = () => {
       title: "Products",
       icon: <BsCardText />,
       arrow: <MdOutlineArrowForwardIos />,
+      href: "/vendor/products",
     },
     {
       title: "Add New Products",
       icon: <BsUpload />,
       arrow: <MdOutlineArrowForwardIos />,
+      href: "/vendor/products/add-product",
     },
     {
       title: "Orders",
       icon: <BsCart2 />,
       arrow: <MdOutlineArrowForwardIos />,
+      href: "/vendor/orders",
     },
     {
       title: "Accounts Settings",
       icon: <AiOutlineSetting />,
       arrow: <MdOutlineArrowForwardIos />,
+      href: "/vendor/account-settings",
     },
   ];
   const apps = [
@@ -71,6 +78,7 @@ const VendorSideBarNavigation = () => {
           <ul className="text-gray-500">
             {dashboard.map((link, index) => (
               <li
+                onClick={() => router.push(link.href)}
                 key={index}
                 className="group flex cursor-pointer items-center justify-between py-3 px-5 text-sm transition duration-200 ease-in-out hover:bg-slate-200 hover:text-black"
               >
@@ -92,6 +100,7 @@ const VendorSideBarNavigation = () => {
           <ul className="text-gray-500">
             {pages.map((link, index) => (
               <li
+                onClick={() => router.push(link.href)}
                 key={index}
                 className="group flex cursor-pointer items-center justify-between py-3 px-5 text-sm transition duration-200 ease-in-out hover:bg-slate-200 hover:text-black"
               >
@@ -113,6 +122,7 @@ const VendorSideBarNavigation = () => {
           <ul className="text-gray-500">
             {apps.map((link, index) => (
               <li
+                // onClick={() => router.push(link.href)}
                 key={index}
                 className="group flex cursor-pointer items-center justify-between py-3 px-5 text-sm transition duration-200 ease-in-out hover:bg-slate-200 hover:text-black"
               >
